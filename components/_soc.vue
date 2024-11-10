c7ba796a01a2a648d2d133fa8c03745efd6058bb
anastasiiaxfr <anastasiiaberest@gmail.com>
2024-11-10 21:31:21 +0200

    footer

---
+++ components/_soc.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="soc">
+    <NuxtLink v-for="(i, ind) in soc" :key="ind" :to="i.url">
+      <NuxtImg :src="i.icon" :alt="i.label" width="24" height="24" />
+    </NuxtLink>
+  </nav>
+</template>
+
+<script setup>
+const soc = [
+  { icon: "/icon/s1.svg", label: "s1", url: "/" },
+  { icon: "/icon/s2.svg", label: "s2", url: "/" },
+  { icon: "/icon/s3.svg", label: "s3", url: "/" },
+  { icon: "/icon/s4.svg", label: "s4", url: "/" },
+];
+</script>
--- components/footer.vue
+++ components/footer.vue
@@ -1,3 +1,81 @@
 <template>
-  <h1>footer</h1>
+  <footer class="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="footer-info">
+          <NuxtLink to="/" class="logo">
+            <img :src="logoImg" alt="Logo" />
+          </NuxtLink>
+          <div class="footer-note">
+            Our mission is to empower individuals and businesses by providing them with the financial resources they
+            need to achieve their goals.
+          </div>
+          <Soc />
+        </div>
+        <div>
+          <div class="footer-nav">
+            <div>
+              <div class="footer-nav-title">OUR SERVICES</div>
+              <hr class="divider" />
+              <ul>
+                <li v-for="(i, ind) in nav" :key="ind">
+                  <a :href="i.url">{{ i.label }}</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <div class="footer-nav-title">CONTACT US</div>
+              <hr class="divider" />
+              <nav class="contacts">
+                <p v-for="(i, ind) in contacts" :key="ind">
+                  <a href="i.url">
+                    <NuxtImg :src="i.icon" :alt="i.label" width="24" height="24" /><span v-html="i.label"></span>
+                  </a>
+                </p>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-btm">
+      <div class="container">
+        <nav>
+          <NuxtLink to="/terms">Terms </NuxtLink>
+          <NuxtLink to="/policy">Privacy Policy </NuxtLink>
+          <NuxtLink to="/cookie">Cookie Policy </NuxtLink>
+        </nav>
+      </div>
+    </div>
+  </footer>
 </template>
+
+<script setup>
+import logoImg from "~/assets/img/logo-w.svg";
+import Soc from "~/components/_soc.vue";
+
+const nav = [
+  { url: "/", label: "Personal loan" },
+  { url: "/", label: "Business loan" },
+  { url: "/", label: "Education loan" },
+  { url: "/", label: "Auto loan" },
+];
+
+const contacts = [
+  {
+    icon: "/icon/s5.svg",
+    url: "tel:+919999999999",
+    label: "+91 99999 99999",
+  },
+  {
+    icon: "/icon/s6.svg",
+    url: "mailto:xyzfh5@gmail.com",
+    label: "xyzfh5@gmail.com",
+  },
+  {
+    icon: "/icon/s7.svg",
+    url: "/",
+    label: "Address line-1 <br>Address line-2, <br>city, state-pin code.",
+  },
+];
+</script>
